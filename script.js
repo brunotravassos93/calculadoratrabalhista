1fec9ae51962c39aa4435319e02f910a64ef3715
Bruno Travassos <103973043+brunotravassos93@users.noreply.github.com>
2022-08-29 10:32:42 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,20 +4,36 @@ select("#feriadoSim").addEventListener("change", () =>
    select("#inputFeriado").classList.toggle("hidden")
 );
 
-select("#button").addEventListener("click", (event) => {
-   event.preventDefault();
-   let horasTrabalhadas = select('#inputSemFeriado').value;
-   let valorHora = select('#valorHora').value;
-   let resultado;
+select("#button").addEventListener('click', (e) => {
+   e.preventDefault();
+   calcular();
+})
 
-   if (horasTrabalhadas <= 40) {
-      resultado = horasTrabalhadas * valorHora;
-   } else {
-      resultado = 40 * valorHora + (horasTrabalhadas - 40) * valorHora * 1.5;
+select("#reset").addEventListener('click', (e) => {
+   document.getElementById('resultado').textContent = '';
+})
+
+function calcular() {
+   let horasTrabalhadas = document.querySelector('#horasSemFeriado').value;
+   let valorHora = document.querySelector('#valorHora').value;
+   let resultado = '';
+   let horasFeriado = select('#horasFeriado').value;
+
+
+   switch ((select("#horasFeriado").classList.contains("hidden"))) {
+      case (select("#horasFeriado").classList.contains("hidden")):
+         if(horasTrabalhadas <= 40) {
+            resultado = horasTrabalhadas * valorHora;
+         } else {
+            resultado = (40 * valorHora) + ((horasTrabalhadas - 40) * (valorHora * 1.5));
+         }
+      case (!select("#horasFeriado").classList.contains("hidden")):
+         if(horasTrabalhadas <= 40) {
+            resultado = (horasTrabalhadas * valorHora) + (horasFeriado * (valorHora * 2));
+         } else {
+            resultado = (40 * valorHora) + ((horasTrabalhadas - 40) * (valorHora * 1.5)) + (horasFeriado * (valorHora * 2));
+         }
    }
 
-   if (!select("#inputFeriado").classList.contains("hidden")) {
-      resultado += select("#valorHoraFeriado").value * 2;
-   }
    select('#resultado').textContent = resultado;
-});
+}
