00ff31e2537a819dcef697416646716e0b3c6928
Bruno Travassos <103973043+brunotravassos93@users.noreply.github.com>
2022-08-29 10:58:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,23 +3,27 @@ var select = (selector) => document.querySelector(selector);
 select("#feriadoSim").addEventListener("change", () =>
    select("#inputFeriado").classList.toggle("hidden")
 );
-
+   
 select("#button").addEventListener('click', (e) => {
    e.preventDefault();
    calcular();
 })
-
-select("#reset").addEventListener('click', (e) => {
-   document.getElementById('resultado').textContent = '';
+   
+select("#reset").addEventListener('click', (event) => {
+   event.document.getElementById('resultado').textContent = '';
 })
 
+select("#reset").addEventListener('click', (event) => {
+   document.getElementById("inputFeriado").classList.add('hidden');
+})
+   
 function calcular() {
    let horasTrabalhadas = document.querySelector('#horasSemFeriado').value;
    let valorHora = document.querySelector('#valorHora').value;
    let resultado = '';
    let horasFeriado = select('#horasFeriado').value;
 
-
+   
    switch ((select("#horasFeriado").classList.contains("hidden"))) {
       case (select("#horasFeriado").classList.contains("hidden")):
          if(horasTrabalhadas <= 40) {
@@ -27,13 +31,13 @@ function calcular() {
          } else {
             resultado = (40 * valorHora) + ((horasTrabalhadas - 40) * (valorHora * 1.5));
          }
-      case (!select("#horasFeriado").classList.contains("hidden")):
-         if(horasTrabalhadas <= 40) {
-            resultado = (horasTrabalhadas * valorHora) + (horasFeriado * (valorHora * 2));
-         } else {
-            resultado = (40 * valorHora) + ((horasTrabalhadas - 40) * (valorHora * 1.5)) + (horasFeriado * (valorHora * 2));
+         case (!select("#horasFeriado").classList.contains("hidden")):
+            if(horasTrabalhadas <= 40) {
+               resultado = (horasTrabalhadas * valorHora) + (horasFeriado * (valorHora * 2));
+            } else {
+               resultado = (40 * valorHora) + ((horasTrabalhadas - 40) * (valorHora * 1.5)) + (horasFeriado * (valorHora * 2));
+            }
          }
-   }
-
-   select('#resultado').textContent = resultado;
-}
+         
+         select('#resultado').textContent = resultado;
+      }
